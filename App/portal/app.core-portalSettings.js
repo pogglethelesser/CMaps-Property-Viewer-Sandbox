d8894c3775255fe035c2202a093ffca5f1a0ef2f
Ryan Elley <ryan.elley@ecan.govt.nz>
2016-08-18 11:29:02 +1200

    Removed pages (not really set up properly).

--- App/portal/app.core-portalSettings.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    'use strict';
-
-    angular
-        .module('app.core')
-        .constant('portalSettings', {
-            BASE_PORTAL_URL: 'http://www.arcgis.com/sharing/rest/'
-        });
-})();
