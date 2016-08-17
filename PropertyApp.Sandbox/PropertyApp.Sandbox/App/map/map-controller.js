280f5a93b8c3ef2ac523f442f6eafbad53d70d7d
Ryan Elley <ryan.elley@ecan.govt.nz>
2016-08-18 09:27:19 +1200

    Added a location pin to the map when an address is selected from the type ahead search.

--- PropertyApp.Sandbox/PropertyApp.Sandbox/App/map/map-controller.js
+++ PropertyApp.Sandbox/PropertyApp.Sandbox/App/map/map-controller.js
@@ -15,10 +15,34 @@
         activate();
 
         function activate() {
-            esriLoader.require(['esri/Map','esri/geometry/Point'], function (Map, Point) {
+            esriLoader.require(
+            [
+                'esri/Map',
+                'esri/geometry/Point',
+                'esri/Graphic',
+                'esri/layers/GraphicsLayer',
+                'esri/symbols/Symbol',
+                'esri/symbols/PictureMarkerSymbol',
+                'esri/symbols/support/jsonUtils'
+            ],
+
+            function (Map, Point, Graphic, GraphicsLayer, Symbol, PictureMarkerSymbol, symbolJsonUtils) {
+                // Create the map - use a basic map at this stage (could be swapped out at some point to use a webmap from ArcGIS.com)
                 vm.map = new Map({
                     basemap: 'streets'
                 });
+
+                // Create the location point marker symbol
+                vm.locationMarkerSymbol = PictureMarkerSymbol({
+                    url: "/content/images/locationPin.png",
+                    width: "33px",
+                    height: "54px",
+                    yoffset: "27px"
+                });
+
+                // Add graphics layer
+                vm.locationGraphicsLayer = new GraphicsLayer();
+                vm.map.add(vm.locationGraphicsLayer);
 
                 // Listener for location change event
                 $rootScope.$on(events.CHANGE_LOCATION, function (evt, data) {
@@ -36,15 +60,33 @@
                             case 'VAL':
                                 if (vm.mapView.scale > 10000)
                                     vm.mapView.scale = 10000;
+
+                                // Update the location marker
+                                vm.updateLocationPoint(pt, data.location);
                                 break;
 
                             default:
+                                // Clear any existing location markers
+                                vm.locationGraphicsLayer.removeAll();
                                 if (vm.mapView.scale > 25000)
                                     vm.mapView.scale = 25000;
                                 break;
                         }
                     }
                 });
+
+                // Add location graphic update function
+                vm.updateLocationPoint = function (pt, locationname) {
+                    // Clear the existing graphic
+                    vm.locationGraphicsLayer.removeAll();
+
+                    // add a new graphic using the pt as a location and the location marker symbol
+                    var graphic = new Graphic(
+                        { geometry: pt, symbol: vm.locationMarkerSymbol, attributes: { address: locationname } }
+                        );
+
+                    vm.locationGraphicsLayer.add(graphic);
+                };
             });
 
             vm.onViewCreated = function (view) {
